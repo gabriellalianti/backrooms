001ed13df7ec6200855bc1f71be434989c9e8e4d
Gabriella Lianti <134288268+gabriellalianti@users.noreply.github.com>
2026-09-15 20:45:34 +1000

    fix(deploy): fix production build and routing

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  plugins: [react(), cloudflare()],
-});
+export default defineConfig(({ mode }) => {
+  const configPath =
+    mode === "cloudflare-production"
+      ? "./wrangler.production.jsonc"
+      : "./wrangler.jsonc";
 
+  return {
+    plugins: [react(), cloudflare({ configPath })],
+  };
+});
